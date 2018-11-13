c5d4f5e52514898623593d230f853f720d97c11c
Jon Pawelko <JonPawelko@yahoo.com>
2018-11-13 15:09:46 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Enter your js code Here
+
+var x;
+var firstName;
+var result;
+
+x = 5;
+firstName = "Jon";
+result = true;  //boolean
+
+// alert(x);  // week 1 code
+// alert (name);
+// alert(result);
+
+console.log("Hello");   // week 2 code
+console.log("My friend is " + firstName);
+console.log(x*x);
+
+// remove the comment identifier one at a time to test out the functionality
+
+// document.body.innerHTML = "Hellllllllo";
+
+// document.getElementById("jon").innerHTML = "I changed this";
+
+// alert(document.getElementById("jon").innerHTML);
+
+alert(Math.floor(Math.random()*6)+1);  // when I want a number between 1 and 6
